f9e4ab3595596222b1873fcf3678af2c8953660e
ASyper <adriansyper2@o2.pl>
2022-05-26 21:43:36 +0200

    A.Syper

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
 int main()
-{ double liczba10; // liczba z u³amkiem dziesiêtnym
+{ double liczba10; 
  double ulamek;
     int n;
     int t[10]={0};
-    int u[100]={0}; //na ulamek binarny
+    int u[100]={0}; 
     int i=9;
-cout<<"Podaj liczbe";
-cin>>liczba10; //wczytanie liczby z ulamkiem
+cout<<"Podaj Liczbe";
+cin>>liczba10; 
 //cin>>n;
-n=(int) liczba10; //pobieramy czesc calkowita
-ulamek=liczba10 - n; // czesc ulamkowa
-while(n>0){ //petla do czesci calkowitej
+n=(int) liczba10; 
+ulamek=liczba10 - n; 
+while(n>0){ 
     t[i]=n%2;
     n=n/2;
     i--;}
-for(int i=0; i<100; i++){ //petla dla czesci ulamkowej
+for(int i=0; i<100; i++){ 
     ulamek=2*ulamek;
 if(ulamek>=1) {u[i]=1; ulamek=ulamek-1;}
     else u[i]=0;
 }
-cout<<"binarnie to:";
+cout<<"Binarnie To:";
 for(i=0;i<10;i++)cout<<t[i];
 cout<<".";
 for(i=0;i<100;i++)cout<<u[i];
